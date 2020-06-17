52c8e127f1eb6e258d3b09971d8cb36500ed29b8
cecilialourenco <66224234+cecilialourenco@users.noreply.github.com>
2020-06-17 13:07:12 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -3,10 +3,13 @@ notas = []
 
 nota1 = float(input('Digite a primeira nota: '))
 notas.append(nota1)
+
 nota2 = float(input('Digite a segunda nota: '))
 notas.append(nota2)
+
 nota3 = float(input('Digite a terceira nota: '))
 notas.append(nota3)
+
 nota4 = float(input('Digite a quarta nota: '))
 notas.append(nota4)
 
