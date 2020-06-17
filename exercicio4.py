960231651566a35e94f9e075124280ef3ddf1bb9
Cecília Lourenco <ceciliamblourenco@gmail.com>
2020-06-17 20:28:35 -0300

    Exercicios

--- exercicio4.py
+++ exercicio4.py
@@ -1,12 +1,25 @@
-'''Programa que utiliza um dict que lê dados de entrada do usuário. O usuário
-deve entrar com os dados de uma pessoa como nome, idade e cidade onde mora.'''
+'''1 - Programa que utiliza um dict que lê dados de entrada do usuário.
+O usuário deve entrar com os dados de uma pessoa como nome, idade e cidade onde
+mora. Esses dados deverão ser adicionados em uma lista.'''
+'''2 - Perguntar ao usuário se ele deseja adicionar uma nova pessoa. Após
+adicionar dados de algumas pessoas, todos os dados de cada pessoa terão que ser
+impressos de forma organizada.'''
 
-nome = input('Digite o seu nome: ')
+pergunta = 'sim'
+pessoas = []
 
-idade = int(input('Digite sua idade: '))
+while (pergunta == 'sim'):
+    nome = input('Digite o seu nome: ')
+    idade = int(input('Digite sua idade: '))
+    cidade = input('Digite a sua cidade: ')
+    dados = {'Nome': nome, 'Idade': idade, 'Cidade': cidade}
 
-cidade = input('Digite a sua cidade: ')
+    pessoas.append(dados)
 
-dados = {'Nome':nome, 'Idade':idade, 'Cidade':cidade}
+    pergunta = input('Deseja adicionar uma nova pessoa? ')
 
-print(dados)
+for pessoa in pessoas:
+    for chave, valor in pessoa.items():
+        print(chave + ":", valor)
+
+    print('')
