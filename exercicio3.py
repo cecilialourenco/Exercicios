9843c9d6ef0b4537ec5482acf78a09a310ee2acd
cecilialourenco <66224234+cecilialourenco@users.noreply.github.com>
2020-06-17 12:59:58 -0300

    Add files via upload

---
+++ exercicio3.py
@@ -0,0 +1,15 @@
+'''Faça um programa que leia 4 notas, mostre as notas e a média na tela.'''
+notas = []
+
+nota1 = float(input('Digite a primeira nota: '))
+notas.append(nota1)
+nota2 = float(input('Digite a segunda nota: '))
+notas.append(nota2)
+nota3 = float(input('Digite a terceira nota: '))
+notas.append(nota3)
+nota4 = float(input('Digite a quarta nota: '))
+notas.append(nota4)
+
+print(notas)
+
+media = float(input('A média é: {}'. format(sum(notas)/len(notas))))
